9f620d437543b331484915afdaa36c02ece4be58
vanshanand34 <anandvansh34@gmail.com>
2024-06-08 23:59:40 +0530

    Modified the arrow buttons for better look

--- src/components/Homeque.js
+++ src/components/Homeque.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
 import bgpic from "./Homebg.png";
-import icon from "./Icon.svg";
 import "../App.css";
 import { Link, useLocation } from "react-router-dom";
 import AudioRead from "./AudioRead";
@@ -111,25 +110,23 @@ const Homeque = () => {
           <img className="w-full max-w-xl mb-8" src={bgpic} alt="Background" />
           <div className="flex justify-between items-center w-full max-w-xl mb-8">
             <Link
-              to={`/api/chapter/${chap}/shlok/${ver - 1}`}
-              className="transform rotate-180"
-            >
-              <br></br>
-              
-              <img
-                className="w-12 h-8 sm:w-10 sm:h-8 opacity-100 hover:opacity-50 duration-300 cursor-pointer"
-                onClick={handlePrevious}
-                src={icon}
-                alt="Previous"
-              />
+              to={`/api/chapter/${chap}/shlok/${ver - 1}`}>          
+              <span className="w-12 h-8 sm:w-10 sm:h-8 opacity-80 hover:opacity-100 duration-300 cursor-pointer"
+              onClick={handlePrevious}
+              alt="Previous">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="indigo" class="bi bi-arrow-left-circle-fill hover:fill-white " viewBox="0 0 16 16">
+                  <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+                </svg>          
+              </span>
             </Link>
             <Link to={`/api/chapter/${chap}/shlok/${ver + 1}`}>
-              <img
-                className="w-12 h-8 sm:w-10 sm:h-8 opacity-20 hover:opacity-50 duration-300 cursor-pointer"
-                onClick={handleNext}
-                src={icon}
-                alt="Next"
-              />
+              <span
+                className="w-12 h-8 sm:w-10 sm:h-8 opacity-80 hover:opacity-100 duration-300 cursor-pointer"
+                onClick={handleNext}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="indigo" className="bi bi-arrow-right-circle-fill hover:fill-white " viewBox="0 0 16 16">
+              <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+              </svg>
+              </span>
             </Link>
           </div>
           <div className="text-center w-full">
